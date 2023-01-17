9c8a8491623832b87d22d5e5dd2543930c099417
hyarionh <hyarion.bathurst@gmail.com>
2023-01-17 09:28:13 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { PageComponent } from './page/page.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
